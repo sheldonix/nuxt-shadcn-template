689560e63d33295e307c33d001bce12d2a539399
sheldonix <873651+sheldonix@users.noreply.github.com>
2025-12-24 01:32:36 +0800

    pnpm dlx nuxi@latest module add shadcn-nuxt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,8 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   modules: [
+    '@nuxt/eslint',
     '@nuxtjs/tailwindcss',
-    '@nuxt/eslint'
+    'shadcn-nuxt'
   ]
 })
