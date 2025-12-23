6141d0c591434171bb328b5c8b79cac913c80ced
sheldonix <873651+sheldonix@users.noreply.github.com>
2025-12-24 01:28:08 +0800

    Add '@nuxtjs/tailwindcss' module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,8 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint']
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@nuxt/eslint'
+  ]
 })
